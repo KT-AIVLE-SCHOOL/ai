630dda74331f54a6b69ed6c7fcbf54b6fe191e5b
jaehjoo <wnwoduq@naver.com>
2025-01-17 14:04:29 +0900

    Feat: echo server

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN pip install -r requirements.txt
 
 COPY ./server_app /app
 
-EXPOSE 8000
+EXPOSE 8001
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8001", "--reload"]
--- server_app/main.py
+++ server_app/main.py
@@ -1,3 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from fastapi.responses import JSONResponse
+from typing import Annotated
+from pydantic import BaseModel
+import requests
+import os
 
 app = FastAPI()
+path = os.getcwd()
+
+class ChatRequest(BaseModel):
+    question: str
+
+@app.get('/api/getAnswer')
+async def getAnswer(req: Annotated[ChatRequest, Query()]):
+   response = {'answer': req.question}
+   return JSONResponse(content=response)
